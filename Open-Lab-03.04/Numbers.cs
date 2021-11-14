3386e5c3e7d9e33cea66f43a4125fa45aebf100a
AdamHaluska <adam.haluska11@gmail.com>
2021-11-14 21:11:43 +0100

    hotovo!

--- Open-Lab-03.04/Numbers.cs
+++ Open-Lab-03.04/Numbers.cs
@@ -4,9 +4,12 @@ namespace Open_Lab_03._04
 {
     public class Numbers
     {
-        public string EvenOrOdd(int number)
+         if (number % 2 == 0)
+                return "even";
+            
         {
-            throw new NotImplementedException();
+            else
+                return "odd";
         }
     }
 }
